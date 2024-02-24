2de4c50a86bc248b53b91b61e98dca13a9389a09
Furkan Cetinalp <cetinalp.furkan0@gmail.com>
2024-02-24 03:07:14 +0300

    update

--- src/ecommerce_frontend/src/App.jsx
+++ src/ecommerce_frontend/src/App.jsx
@@ -5,24 +5,24 @@ import Navbar from './components/Navbar';
 import Signin from './pages/Auth/Signin';
 import Products from './pages/Products';
 function App() {
-  const [greeting, setGreeting] = useState('');
-  ecommerce_backend.greet("furkan").then((item) => {
-    console.log(item);
-  });
+  // const [greeting, setGreeting] = useState('');
+  // ecommerce_backend.greet("furkan").then((item) => {
+  //   console.log(item);
+  // });
 
 
   function handleSubmit(event) {
     event.preventDefault();
     const name = event.target.elements.name.value;
-    ecommerce_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    let result;
-    ecommerce_backend.greet("furkan").then((item) => {
-      console.log(item);
-      result = item;
-    });
-    console.log(result);
+    // ecommerce_backend.greet(name).then((greeting) => {
+    //   setGreeting(greeting);
+    // });
+    // let result;
+    // ecommerce_backend.greet("furkan").then((item) => {
+    //   console.log(item);
+    //   result = item;
+    // });
+    // console.log(result);
     // console.log("greeting data", data);
     return false;
   }
