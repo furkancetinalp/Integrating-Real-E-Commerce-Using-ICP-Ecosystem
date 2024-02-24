3db1ccbc7585eeaff6c853565f6b5076e57c0da8
Furkan Cetinalp <cetinalp.furkan0@gmail.com>
2024-02-24 14:56:39 +0300

    update products page

--- src/ecommerce_frontend/src/pages/Products/index.jsx
+++ src/ecommerce_frontend/src/pages/Products/index.jsx
@@ -11,44 +11,44 @@ import { import_products, get_all_products_from_canister } from '../../api';
 function Products() {
     const [productDetail, setproductDetail] = React.useState([]);
 
-    let userid=localStorage.getItem("userid");
+    let userid = localStorage.getItem("userid");
     let token = localStorage.getItem("token");
     const [products, setProducts] = React.useState([]);
     React.useEffect(() => {
-        (async () => {
-            const getproducts = async () => {
-                const imp = await ecommerce_backend.import_products(userid,token)
-                const productList = await ecommerce_backend.get_all_products_from_canister().then(res => res.json())
-                    .then(data => setProducts(data[0]))
-                    .catch(err => console.error("Custom err: ", err))
-            }
-        })
-    },[products]);
+        {
+            const productList = ecommerce_backend.get_all_products_from_canister()
+                .then(data => setProducts(data[0]))
+                .catch(err => console.error("Custom err: ", err))
+        }
+    }, []);
 
     async function handleSubmit(event) {
         event.preventDefault();
-        const imp = await ecommerce_backend.import_products(userid,token)
-        await ecommerce_backend.get_all_products_from_canister().then((data) => {
-            setProducts(data[0]);
-        });
+        const imp = await ecommerce_backend.import_products(userid, token)
 
-        
+        // await ecommerce_backend.get_all_products_from_canister().then((data) => {
+        //     setProducts(data[0]);
+        // });
+
+
         return false;
     }
 
     return (
-        <div>
-            <form action="#" onSubmit={handleSubmit}>
-                <button type="submit">Show Products!</button>
+        <Box mx='10' my='10'>
+            <form style={{float: 'right'}} action="#" onSubmit={handleSubmit}>
+                <Button colorScheme='orange'  type="submit">Import Products</Button>
             </form>
-            <Grid templateColumns="repeat(3,1fr)" gap={4}>
+            <br/>
+            <br/>
+            <Grid mt='25' templateColumns="repeat(3,1fr)" gap={4}>
                 {products != undefined ? products.map((item, key) => {
                     return (
                         <Card key={key} item={item} />
                     )
                 }) : <NoContent204 />}
             </Grid>
-        </div>
+        </Box>
     )
 }
 
